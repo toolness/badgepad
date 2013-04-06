157f46568b4a254593215536a1389635e37c6f93
Atul Varma <varmaa@gmail.com>
2013-04-05 21:14:32 -0400

    removed mock, using sample project dir instead.

--- tests/test_project.py
+++ tests/test_project.py
@@ -1,27 +1,21 @@
+import os
 import unittest
-
-from mock import patch, mock_open
 
 from badgepad.project import Project
 
-CFG_YML = """\
-issuer:
-  url: http://foo.org
-recipients:
-  foo: Foo <foo@bar.org>
-"""
+path = lambda *x: os.path.join(ROOT, *x)
+ROOT = os.path.dirname(os.path.abspath(__file__))
+SAMPLE_PROJECT = path('sample-project')
 
 class ProjectTests(unittest.TestCase):
-    @patch('badgepad.project.open', mock_open(read_data=CFG_YML), create=True)
     def testRecipientsAreParsed(self):
-        proj = Project('.')
+        proj = Project(SAMPLE_PROJECT)
         self.assertEqual(proj.config['recipients']['foo'].name, 'Foo')
         self.assertEqual(proj.config['recipients']['foo'].email,
                          'foo@bar.org')
 
-    @patch('badgepad.project.open', mock_open(read_data=CFG_YML), create=True)
     def testSetBaseUrlAddsSlash(self):
-        proj = Project('.')
+        proj = Project(SAMPLE_PROJECT)
         self.assertEqual(proj.config['issuer']['url'], 'http://foo.org/')
         proj.set_base_url('http://p/blah/')
         self.assertEqual(proj.config['issuer']['url'], 'http://p/blah/')
