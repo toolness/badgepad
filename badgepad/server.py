1538421423f80ea69ca7a973b77f806daff45785
Atul Varma <varmaa@gmail.com>
2013-04-08 00:14:59 -0400

    another minimock doctest for server module.

--- badgepad/server.py
+++ badgepad/server.py
@@ -28,28 +28,31 @@ def start_file_server(ip, port):
     print "serving at port %s on %s" % (port, ip)
     httpd.serve_forever()
 
+def auto_rebuilder(root_dir, dest_dir):
+    laststate = None
+    while True:
+        currstate = get_dir_state(root_dir)
+        if currstate != laststate:
+            laststate = currstate
+            sys.stdout.write("rebuilding website... ")
+            sys.stdout.flush()
+            try:
+                build_website(Project(root_dir), dest_dir=dest_dir)
+                sys.stdout.write("done.\n")
+            except Exception:
+                traceback.print_exc(file=sys.stdout)
+        yield
+
 def start_auto_rebuild_server(root_dir, ip, port):
-    dirname = tempfile.mkdtemp()
+    dest_dir = tempfile.mkdtemp()
     thread = threading.Thread(target=start_file_server,
                               kwargs=dict(ip=ip, port=port))
     thread.daemon = True
     thread.start()
-    laststate = None
     try:
-        while True:
-            currstate = get_dir_state(root_dir)
-            if currstate != laststate:
-                laststate = currstate
-                project = Project(root_dir)
-                print "rebuilding website... ",
-                sys.stdout.flush()
-                try:
-                    build_website(project, dest_dir=dirname)
-                    print "done."
-                except Exception:
-                    traceback.print_exc()
-                os.chdir(dirname)
+        for _ in auto_rebuilder(root_dir, dest_dir):
+            os.chdir(dest_dir)
             time.sleep(1)
     finally:
-        if os.path.exists(dirname):
-            shutil.rmtree(dirname)
+        if os.path.exists(dest_dir):
+            shutil.rmtree(dest_dir)
--- tests/test_server.py
+++ tests/test_server.py
@@ -7,17 +7,90 @@ from minimock import mock, Mock, restore
 
 from badgepad import server
 from badgepad.server import get_dir_state
+from .test_project import SAMPLE_PROJECT
 
 def load_tests(loader, tests, ignore):
     tests.addTests(doctest.DocTestSuite())
     return tests
+
+def test_start_auto_rebuild_server():
+    """
+    >>> mock('server.threading.Thread', returns=Mock('thread'))
+    >>> mock('server.time.sleep')
+    >>> mock('server.tempfile.mkdtemp', returns='temp')
+    >>> mock('server.os.chdir')
+    >>> mock('server.auto_rebuilder', returns=[1, 2])
+    >>> mock('server.shutil.rmtree')
+    >>> mock('server.os.path.exists', returns=True)
+
+    >>> server.start_auto_rebuild_server(
+    ...     'root',
+    ...     '127.0.0.1',
+    ...     3000
+    ... )                                                # doctest: +ELLIPSIS
+    Called server.tempfile.mkdtemp()
+    Called server.threading.Thread(
+        kwargs={'ip': '127.0.0.1', 'port': 3000},
+        target=<function start_file_server at ...>)
+    Called thread.start()
+    Called server.auto_rebuilder('root', 'temp')
+    Called server.os.chdir('temp')
+    Called server.time.sleep(1)
+    Called server.os.chdir('temp')
+    Called server.time.sleep(1)
+    Called server.os.path.exists('temp')
+    Called server.shutil.rmtree('temp')
+
+    >>> restore()
+    """
+
+    pass
+
+def test_auto_rebuilder():
+    """
+    >>> mock('server.build_website')
+    >>> mock('server.get_dir_state', returns='abc')
+    >>> mock('server.Project', returns='proj')
+
+    >>> ar = server.auto_rebuilder('root', 'dest')
+    >>> ar.next()
+    Called server.get_dir_state('root')
+    rebuilding website... Called server.Project('root')
+    Called server.build_website('proj', dest_dir='dest')
+    done.
+
+    >>> ar.next()
+    Called server.get_dir_state('root')
+
+    >>> server.get_dir_state.mock_returns = 'def'
+    >>> server.build_website.mock_raises = Exception('hmph')
+    >>> ar.next()                                        # doctest: +ELLIPSIS
+    Called server.get_dir_state('root')
+    rebuilding website... Called server.Project('root')
+    Called server.build_website('proj', dest_dir='dest')
+    Traceback (most recent call last):
+    ...
+    Exception: hmph
+
+    >>> server.get_dir_state.mock_returns = 'ghi'
+    >>> server.build_website.mock_raises = None
+    >>> ar.next()
+    Called server.get_dir_state('root')
+    rebuilding website... Called server.Project('root')
+    Called server.build_website('proj', dest_dir='dest')
+    done.
+
+    >>> restore()
+    """
+
+    pass
 
 def test_start_file_server_works():
     """
     >>> import SocketServer
     >>> mock('SocketServer.TCPServer')
     >>> SocketServer.TCPServer.mock_returns = Mock('httpd')
-    >>> server.start_file_server('127.0.0.1', 8000)       # doctest: +ELLIPSIS
+    >>> server.start_file_server('127.0.0.1', 8000)     # doctest: +ELLIPSIS
     Called SocketServer.TCPServer(
         ('127.0.0.1', 8000),
         <class SimpleHTTPServer.SimpleHTTPRequestHandler at ...>)
